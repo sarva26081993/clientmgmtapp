50ce7d8097eef0408d0fcad9d91c387a768c69c1
unknown <kadam.sarvajeet7@gmail.com>
2025-01-12 18:25:52 +0530

    Added error handling

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -14,7 +14,9 @@ export class RegisterComponent implements OnInit {
   useremail: string = '';
   address: string = '';
   userpassword: string = '';
+  cnfpassword: string = '';
   message: string = '';
+  errorFlag: boolean = false;
 
   constructor(
     private http: HttpClient,
@@ -25,21 +27,30 @@ export class RegisterComponent implements OnInit {
   ngOnInit(): void {}
 
   registerClient() {
-    const client = {
-      name: this.name,
-      useremail: this.useremail,
-      address: this.address,
-      userpassword: this.userpassword,
-    };
-
-    this.http.post('http://localhost:3000/registerClient', client).subscribe(
-      (response: any) => {
-        this.message = response.message;
-        this.router.navigate(['/displayClient'], { state: { client } });
-      },
-      (error) => {
-        console.error('Error in registering client', error);
-      }
-    );
+    if (
+      this.name == '' ||
+      this.useremail == '' ||
+      this.address == '' ||
+      this.userpassword == '' ||
+      this.cnfpassword == ''
+    ) {
+      this.errorFlag = true;
+    } else {
+      const client = {
+        name: this.name,
+        useremail: this.useremail,
+        address: this.address,
+        userpassword: this.userpassword,
+      };
+      this.http.post('http://localhost:3000/registerClient', client).subscribe(
+        (response: any) => {
+          this.message = response.message;
+          this.router.navigate(['/displayClient'], { state: { client } });
+        },
+        (error) => {
+          console.error('Error in registering client', error);
+        }
+      );
+    }
   }
 }
